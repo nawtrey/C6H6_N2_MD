54b24ce707f3330be45efe107690def2964f7506
Nik Awtrey <nawtrey@asu.edu>
2019-04-16 12:32:24 -0700

    Added comments to functions.py and fixed V_LJ values

--- Work/functions.py
+++ Work/functions.py
@@ -10,23 +10,45 @@
 
 import numpy as np
 
-m_C     = 12.0107           # amu; mass of Carbon
-m_H     = 1.00794           # amu; mass of Hydrogen
 kB      = 1.3806488e-23     # J/K; Bolzmann's constant
-eps     = 1.654e-21         # J
-sigma   = 3.41e-10          # m
 
 #========================================================
 #============= Functions ================================
 #========================================================
-# === For an Nx4 array of input: (vx, vy, vz, mass) ===
 
 def random_velocities(N):
-    """Takes a number of particles to create an array of random velocities"""
+    """
+    Takes a number of particles to create an array of random velocities
+
+    Parameters
+    ----------
+    N : integer
+        Number of atoms in the system
+
+    Returns
+    -------
+    Velocities : array
+        (Nx3) array of random velocities (Vx, Vy, Vz)
+    """
     return np.random.rand(N, 3) - 0.5
 
 def instantaneous_temperature(vels, masses):
-    """Calculates the instantaneous temperature of the molecule for a single time step"""
+    """
+    Calculates the instantaneous temperature of the system for a single time step
+    
+    Parameters
+    ----------
+    vels : array
+        (Nx3) array of velocities for every atom in system
+    masses : array
+        (Nx1) array of all atom masses in system
+
+    Returns
+    -------
+    Temperature : float
+        Value of system temperature in Kelvin
+
+    """
     N = len(vels)
     Nf = 3*N - 6
     return np.sum(vels**2, axis=1) * masses / kB*Nf
@@ -34,40 +56,117 @@ def instantaneous_temperature(vels, masses):
 def kinetic_temperature(vels, masses):
     """
     Calculates the kinetic temperature of the molecule
-    ----------------------
-    kB = 1.3806488e-23 J/K
-    note: PBC simulations Nf = 3N - 3  (translation)
+    --------------------------------------------------
+    Note: PBC simulations Nf = 3N - 3  (translation)
           droplet in vacuo: Nf = 3N - 6 (translation and rotation)
           droplet with external spherical boundary potential: Nf = 3N-3 (rotation)
+
+    Parameters
+    ----------
+    vels : array
+        (Nx3) array of velocities for every atom in system
+    masses : array
+        (Nx1) array of all atom masses in system
+
+    Returns
+    -------
+    Kinetic Temperature : float
     """
     N = len(vels)
     Nf = 3*N - 6
     return np.sum(vels**2)/kB*Nf
 
 def remove_linear_momentum(vels, masses):
+    """
+    Removes linear momentum of entire system of atoms
+
+    Parameters
+    ----------
+    vels : array
+        (Nx3) array of velocities for every atom in system
+    masses : array
+        (Nx1) array of all atom masses in system
+
+    Returns
+    -------
+    Velocities : array
+        (Nx3) array of new velocities (Vx, Vy, Vz)
+    """
     return vels - np.mean(vels*masses, axis=0)
 
 
 def rescale(vels, temperature):
-    """Rescale velocities so that they correspond to temperature T."""
+    """
+    Rescale velocities so that they correspond to temperature T.
+
+    Parameters
+    ----------
+    vels : array
+        (Nx3) array of velocities for every atom in system
+    temperature : float
+        Value of initial system temperature in Kelvin
+
+    Returns
+    -------
+    Velocities : array
+        (Nx3) array of rescaled velocities (Vx, Vy, Vz)
+
+    """
     current_temperature = kinetic_temperature(vels)
     return np.sqrt(temperature/current_temperature) * vels
 
 def V_LJ(positions):
-    """Calculates the potential energy of a single particle
-    Note: here, r_vector is relative to the origin (0, 0, 0)
-    where as r_ij are the radii of i particles to j particles.
     """
+    Calculates the LJ potential energy between a pair of atoms for a single time step
+
+    Parameters
+    ----------
+    positions : array
+        (Nx3) array of positions for every atom in system
+
+    Returns
+    -------
+    Potential Energy : float
+    """
+    epsilon = 1.654e-21/kB  # Kelvin
+    sigma = 0.341           # nm
     r_mag = np.sqrt(np.sum(positions*positions))      # Calculates the magnitude of r_vector
     if r_mag == 0.0:
         return 0
     else:
-        return 4*eps*((sigma/r_mag)**12 - (sigma/r_mag**6))
+        return 4*epsilon*((sigma/r_mag)**12 - (sigma/r_mag**6))
 
 def total_momentum(vels, masses):
-    """Total linear momentum"""
+    """
+    Calculates the total linear momentum of the system of atoms
+
+    Parameters
+    ----------
+    vels : array
+        (Nx3) array of velocities for every atom in system
+    masses : array
+        (Nx1) array of all atom masses in system
+
+    Returns
+    -------
+    Momentum : float
+        Magnitude of the total linear momentum of the system |P|
+    """
     return np.sum(vels*masses, axis=0)
 
 def KE(vels, masses):
-     """Calculates the kinetic energy of a single particle"""
-     return 0.5*masses*np.sum(vels**2, axis = 1)
+    """
+    Calculates the kinetic energy of a single particle for a single time step
+    
+    Parameters
+    ----------
+    vels : array
+        (Nx3) array of velocities for every atom in system
+    masses : array
+        (Nx1) array of all atom masses in system
+
+    Returns
+    -------
+    Kinetic Energy : float
+    """
+    return 0.5*masses*np.sum(vels**2, axis = 1)
